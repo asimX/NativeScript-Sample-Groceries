f6d930f0e70620c64adf5b593967cda2d177c92a
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-09-08 11:47:52 +0300

    Get Groceries Angular from the NativeScript repo

--- Groceries/karma.conf.js
+++ Groceries/karma.conf.js
@@ -1,5 +1,6 @@
 module.exports = function(config) {
   config.set({
+    browserNoActivityTimeout: 40000,
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -7,7 +8,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
@@ -30,7 +31,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['progress'],
 
 
     // web server port
